47f8e1fca45a0d0d11183e865bdf6dee8da626c4
clementsauvard <clement.sauvard@u-psud.fr>
2016-03-12 19:14:36 +0100

    Settings/Langue/OPSHIT

--- SimongGame/app/src/main/java/cyril/simonggame/view/Setting_View.java
+++ SimongGame/app/src/main/java/cyril/simonggame/view/Setting_View.java
@@ -7,15 +7,19 @@ import android.widget.LinearLayout;
 
 import cyril.simonggame.R;
 import cyril.simonggame.controller.Menu_Controller;
+import cyril.simonggame.controller.Setting_Controller;
 
 /**
  * Created by VirusTwoIUT on 12/03/2016.
  */
 public class Setting_View extends LinearLayout {
     Context context;
+
     public Setting_View(Context context) {
         super(context);
         this.context = context;
+        inflate();
+        bindView();
     }
     private void inflate() {
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -23,5 +27,6 @@ public class Setting_View extends LinearLayout {
     }
     private void bindView(){
         //Futur Objet de la page
+        Setting_Controller controller = new Setting_Controller(this);
     }
 }
