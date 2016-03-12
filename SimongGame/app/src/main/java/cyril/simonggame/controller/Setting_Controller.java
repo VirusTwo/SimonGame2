7d8d7a0d5a018b230b3cee85c6674b0949863179
POKORSKI Alexis <alexis.pokorski@u-psud.fr>
2016-03-12 22:17:03 +0100

    Finalisation activité setting

--- SimongGame/app/src/main/java/cyril/simonggame/controller/Setting_Controller.java
+++ SimongGame/app/src/main/java/cyril/simonggame/controller/Setting_Controller.java
@@ -20,6 +20,8 @@ import cyril.simonggame.view.Setting_View;
 public class Setting_Controller implements View.OnClickListener {
     Setting_View setting_view;
     private int idColor = 0;
+    String numId = "numIdColor";
+    String numLevel = "numLevel";
     public Setting_Controller(Setting_View view){
         this.setting_view = view;
     }
@@ -36,26 +38,73 @@ public class Setting_Controller implements View.OnClickListener {
             case R.id.btColor3 :
                 idColor = 3;
                 break;
+
             case R.id.btRetour :
-                String numId = "numId";
                 compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
                 Intent intent = new Intent(setting_view.getContext(), Menu_Activity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(numId,idColor);
                 setting_view.getContext().startActivity(intent,compat.toBundle());
                 break;
-            case R.id.imgBtEnglish :
-                String languageToLoad  = "fr"; // your language
+
+            case R.id.imgbtEnglish :
+                compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
+                String languageToLoad  = "en"; // your language
                 Locale locale = new Locale(languageToLoad);
                 Locale.setDefault(locale);
+
                 Configuration config = new Configuration();
                 config.locale = locale;
-                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);
+                setting_view.getContext().getResources().updateConfiguration(config, setting_view.getContext().getResources().getDisplayMetrics());
+
+                Intent intent2 = new Intent(setting_view.getContext(), Menu_Activity.class);
+                intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent2.putExtra(numId, idColor);
+                setting_view.getContext().startActivity(intent2, compat.toBundle());
                 break;
+
             case R.id.imgbtFrench :
+                compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
+                String languageToLoadfr  = "fr"; // your language
+                Locale localefr = new Locale(languageToLoadfr);
+                Locale.setDefault(localefr);
+
+                Configuration configfr = new Configuration();
+                configfr.locale = localefr;
+                setting_view.getContext().getResources().updateConfiguration(configfr, setting_view.getContext().getResources().getDisplayMetrics());
+
+                Intent intent3 = new Intent(setting_view.getContext(), Menu_Activity.class);
+                intent3.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent3.putExtra(numId, idColor);
+                setting_view.getContext().startActivity(intent3, compat.toBundle());
+                break;
+
+            case R.id.btLevel1 :
+                compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
+                Intent intent4 = new Intent(setting_view.getContext(), Menu_Activity.class);
+                intent4.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent4.putExtra(numLevel,1);
+                setting_view.getContext().startActivity(intent4,compat.toBundle());
+                break;
+
+            case R.id.btLevel2 :
+                compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
+                Intent intent5 = new Intent(setting_view.getContext(), Menu_Activity.class);
+                intent5.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent5.putExtra(numLevel,2);
+                setting_view.getContext().startActivity(intent5,compat.toBundle());
+                break;
+
+            case R.id.btLevel3 :
+                compat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) setting_view.getContext(), null);
+                Intent intent6 = new Intent(setting_view.getContext(), Menu_Activity.class);
+                intent6.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent6.putExtra(numLevel,3);
+                setting_view.getContext().startActivity(intent6,compat.toBundle());
+                break;
+            
+            case R.id.switch1 :
+
                 break;
         }
     }
--- SimongGame/app/src/main/java/cyril/simonggame/view/Setting_View.java
+++ SimongGame/app/src/main/java/cyril/simonggame/view/Setting_View.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import cyril.simonggame.R;
 import cyril.simonggame.controller.Menu_Controller;
@@ -21,8 +22,13 @@ public class Setting_View extends LinearLayout {
     private Button btColor2;
     private Button btColor3;
     private Button retour;
-    private ImageButton english;
-    private ImageButton french;
+    private Button english;
+    private Button french;
+    private Switch switchDalto;
+    private Button level1;
+    private Button level2;
+    private Button level3;
+
     public Setting_View(Context context) {
         super(context);
         this.context = context;
@@ -41,13 +47,23 @@ public class Setting_View extends LinearLayout {
         btColor2 = (Button) findViewById(R.id.btColor2);
         btColor3 = (Button) findViewById(R.id.btColor3);
         retour = (Button) findViewById(R.id.btRetour);
-        english = (ImageButton) findViewById(R.id.imgBtEnglish);
-        french = (ImageButton) findViewById(R.id.imgbtFrench);
+        english = (Button) findViewById(R.id.imgbtEnglish);
+        french = (Button) findViewById(R.id.imgbtFrench);
+        switchDalto = (Switch) findViewById(R.id.switch1);
+        level1 = (Button) findViewById(R.id.btLevel1);
+        level2 = (Button) findViewById(R.id.btLevel2);
+        level3 = (Button) findViewById(R.id.btLevel3);
 
         btColor1.setOnClickListener(controller);
         btColor2.setOnClickListener(controller);
         btColor3.setOnClickListener(controller);
         retour.setOnClickListener(controller);
+        english.setOnClickListener(controller);
+        french.setOnClickListener(controller);
+        switchDalto.setOnClickListener(controller);
+        level1.setOnClickListener(controller);
+        level2.setOnClickListener(controller);
+        level3.setOnClickListener(controller);
 
     }
 }
